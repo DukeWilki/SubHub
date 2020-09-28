e905983ce0d6ddb97e0b3ad1a3480545d6243e67
Annisa Purbandari <a.purbandari@gmail.com>
2020-09-28 11:15:58 +1000

    added search

--- geolocation.js
+++ geolocation.js
@@ -1,23 +1,56 @@
 
-showPosition = function(position)
-{
-	var lng = position.coords.longitude
-	var lat = position.coords.latitude 
+showPosition = function (position) {
 
-	console.log ( lng ) 
-	console.log ( lat ) 
+	var lng = position.coords.longitude;
+	var lat = position.coords.latitude;
+
+	console.log(lng)
+	console.log(lat)
 
 	document.querySelector("#lng_id").innerHTML = lng;
 	document.querySelector("#lat_id").innerHTML = lat;
 
-	localStorage.setItem("geo_lng" , lng )
-	localStorage.setItem("geo_lat" , lat )
+	localStorage.setItem("geo_lng", lng)
+	localStorage.setItem("geo_lat", lat)
 }
 navigator.geolocation.getCurrentPosition(showPosition);
 
-add_search_term = function()
-{
-	var resto_type = document.querySelector("#search_resto_id").value;
-	localStorage.setItem("geo_resto_type",resto_type)
-	window.location = "display_page.html"
+add_search_term = function () {
+	var radius_select = document.querySelector("#selectRadius").value;
+	localStorage.setItem("geo_radius", radius_select);
+
+	var resto_type = document.querySelector("#selectResto").value;
+	localStorage.setItem("geo_resto_type", resto_type);
+
+	window.location = "display_page.html";
 }
+
+render_dropdown_resto = function () {
+
+	$.ajax({
+		url: 'https://developers.zomato.com/api/v2.1/cuisines?lat=-33.9547434&lon=151.1053804',
+		type: 'GET',
+		headers: {
+			'Accept': 'application/json',
+			'user-key': '143816ae91118f8fc4dfd5768132705f'
+		},
+		success: render_dropdown_resto_callback
+	})
+}
+
+render_dropdown_resto_callback = function (response) {
+	//console.log(response);
+	var cuisines = response.cuisines;
+	for (var idx = 0; idx < cuisines.length; idx++) {
+		var cuisine_item = cuisines[idx];
+		var type_of_resto = cuisine_item.cuisine.cuisine_name;
+
+
+		$('#selectResto').append(
+			'<option value="' + type_of_resto + '">' + type_of_resto + '</option>'
+		)
+	}
+	$('#selectResto').selectpicker('refresh');
+}
+
+render_dropdown_resto();
--- result_page.js
+++ result_page.js
@@ -1,76 +1,100 @@
 
-var lng     = localStorage.getItem("geo_lng")
-var lat     = localStorage.getItem("geo_lat")
+var lng = localStorage.getItem("geo_lng")
+var lat = localStorage.getItem("geo_lat")
 var keyword = localStorage.getItem("geo_resto_type")
-var map     = null;
+var radiusSelect = localStorage.getItem("geo_radius")
+var map = null;
+var bounds = null;
+var resto_bounds = null;
 var service = null;
 
-get_nearby_places = function( current_position , keyword )
-{
-	var param = {
-		location : current_position,
-		rankBy : google.maps.places.RankBy.DISTANCE,
-		keyword : keyword
-	}
-	service = new google.maps.places.PlacesService(map)
-	service.nearbySearch( param , get_nearby_places_callback)
-}
-
-show_markers_on_map = function( result )
-{
-	for (var idx = 0 ; idx < result.length; idx++)
-	{
+show_markers_on_map = function (json_response) {
+	var result = json_response.restaurants;
+	for (var idx = 0; idx < result.length; idx++) {
 		var loc_item = result[idx]
+		var lat = loc_item.restaurant.location.latitude;
+		var lng = loc_item.restaurant.location.longitude;
+		pos = {
+			lat: parseFloat(lat),
+			lng: parseFloat(lng)
+		};
 		var marker = new google.maps.Marker({
-			position: loc_item.geometry.location, 
+			position: pos,
 			map: map,
-			title : loc_item.name,
-			customInfo : loc_item
+			title: loc_item.name,
+			customInfo: loc_item,
+			website: loc_item.url
 		});
-		google.maps.event.addListener(marker, "click" ,
-			function()
-			{
+		bounds.extend(marker.getPosition())
+		resto_bounds.extend(marker.getPosition())
+		google.maps.event.addListener(marker, "click",
+			function (response) {
 				var marker_location = this.position
-				var location_data    = this.customInfo
+				var location_data = this.customInfo
 
-				document.querySelector("#icon_id").innerHTML = "<img src='"+location_data.icon+"'>"
-				document.querySelector("#title_id").innerHTML = location_data.name;
-				document.querySelector("#location_id").innerHTML = location_data.position;
-				document.querySelector("#address").innerHTML = location_data.vicinity
-				document.querySelector("#rating").innerHTML = location_data.rating
+				console.log(location_data);
 
-				map.setCenter( marker_location );
+				document.querySelector("#icon_id").innerHTML = "<img width='250px' src='" + location_data.restaurant.featured_image + "'>"
+				document.querySelector("#title_id").innerHTML = location_data.restaurant.name;
+				document.querySelector("#address").innerHTML = location_data.restaurant.location.address;
+				document.querySelector("#cuisines").innerHTML = location_data.restaurant.cuisines;
+				document.querySelector("#rating").innerHTML = location_data.restaurant.user_rating.aggregate_rating;
+
+				map.setCenter(marker_location);
 			}
 		)
 	}
+	map.fitBounds(bounds);
+	setTimeout(
+		function () {
+			map.fitBounds(resto_bounds);
+			/*
+			setTimeout(
+				function () {
+					map.fitBounds(bounds);
+				}, 2000
+			)
+			*/
+		}, 2000
+	)
 }
 
-get_nearby_places_callback = function(result, status)
-{
-	show_markers_on_map( result );
+get_nearby_places = function (current_position, keyword) {
+	$.ajax({
+		url: 'https://developers.zomato.com/api/v2.1/search?q=chinese&start=0&count=100&lat=-33.9646&lon=151.1010&radius=10000&order=desc',
+		type: 'GET',
+		headers: {
+			'Accept': 'application/json',
+			'user-key': '143816ae91118f8fc4dfd5768132705f'
+		},
+		success: show_markers_on_map
+	})
 }
 
 
-initMap = function()
-{
+
+initMap = function () {
 	///// INIT DISPLAY ////
-	
-	document.querySelector("#keyword_id").innerHTML = keyword; 
 
-        pos = {
-		lat: parseFloat(lat), 
+	document.querySelector("#keyword_id").innerHTML = keyword;
+
+	pos = {
+		lat: parseFloat(lat),
 		lng: parseFloat(lng)
 	};
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pos,
-            zoom: 15,
-	    mapTypeId: 'satellite'
-	});	
-	var marker = new google.maps.Marker({
-		position: pos, 
-		map: map,
-		icon : "https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png"
+	bounds = new google.maps.LatLngBounds();
+	resto_bounds = new google.maps.LatLngBounds();
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: pos,
+		zoom: 15,
+		mapTypeId: 'satellite'
 	});
+	var marker = new google.maps.Marker({
+		position: pos,
+		map: map,
+		icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png"
+	});
+	bounds.extend(marker.getPosition())
 
 	get_nearby_places(
 		pos,
